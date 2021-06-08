a3b2264cc4fdbe9ecf1fa08f1cdf8d08940feaf0
dev-jv <dev.javierleon@gmail.com>
2021-06-08 17:26:44 -0500

    three

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, Output} from '@angular/core';
+import {UserModel} from '../../models/user.model';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,19 @@ import {Component, OnInit, Output} from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  user: UserModel;
+
+  constructor() {
+    this.user = new UserModel();
+    this.user.email = 'drac.1000@gmail.com';
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): any {
+    console.log('Form send');
+    console.log(this.user);
   }
 
 }
