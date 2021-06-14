29ddf64967e7b1f3298cfe56747fcecb6f3136ad
dev-jv <dev.javierleon@gmail.com>
2021-06-13 19:16:46 -0500

    ten

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { UserModel } from '../models/user.model';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,17 @@ export class AuthService {
   private url = 'https://identitytoolkit.googleapis.com/v1/accounts';
   private apikey = 'AIzaSyCKyZt44Rxee_7RUaNXtOwzt0t_IipX7Yg';
 
+  private userToken?: string | null;
+
   // Sign up with email / password
   // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
 
   //   Sign in with email / password
   // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+      this.readToken();
+    }
 
     logOut(): any {
 
@@ -31,8 +36,14 @@ export class AuthService {
         returnSecureToken: true
       };
 
-      return this.http.post(`${this.url}:signInWithPassword?key=${this.apikey}`, authData);
-
+      return this.http.post(`${this.url}:signInWithPassword?key=${this.apikey}`, authData)
+        .pipe(
+          map ((resp: any) => {
+            console.log('RXJS Map');
+            this.saveToken( resp[`idToken`] );
+            return resp;
+          })
+        );
     }
 
     register(user: UserModel): any {
@@ -44,7 +55,29 @@ export class AuthService {
         returnSecureToken: true
       };
 
-      return this.http.post(`${this.url}:signUp?key=${this.apikey}`, authData);
-
+      return this.http.post(`${this.url}:signUp?key=${this.apikey}`, authData)
+        .pipe(
+          map ((resp: any) => {
+            console.log('RXJS Map');
+            this.saveToken( resp[`idToken`] );
+            return resp;
+          })
+        );
     }
+
+    private saveToken( idToken: string ): any {
+      this.userToken = idToken;
+      localStorage.setItem('token', idToken);
+    }
+
+    readToken(): any {
+      if (localStorage.getItem('token')) {
+        this.userToken = localStorage.getItem('token');
+      } else {
+        this.userToken = '';
+      }
+
+      return this.userToken;
+    }
+
 }
